7a8fd064a3151593cad2d137b04616e6419cbf0f
michelablack <36127614+michelablack@users.noreply.github.com>
2020-03-28 17:25:24 +0100

    Second commit added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		//Second addition added
 		System.out.println("First addition added");
 	}
 }
